61680012d4b02a6b6dfdb0c62c78388fe30b261a
KingDanone <lricardolv10@gmail.com>
2025-06-23 12:05:10 -0300

    operação de soma adicionada, e criado tratamento de erros para melhorar o tratamento dos mesmos

---
+++ 04_course-springboot-udemy/src/main/java/br/com/KingDanone/controllers/MathController.java
@@ -0,0 +1,34 @@
+package br.com.KingDanone.controllers;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) throws Exception{
+        if (isNumeric(numberOne) || isNumeric(numberTwo))
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        return convertDouble(numberOne) + convertDouble(numberTwo);
+    }
+
+    private Double convertDouble(String strNumber) throws IllegalArgumentException{
+        if (strNumber == null || strNumber.isEmpty())
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        String number = strNumber.replace(",", ".");
+        return Double.parseDouble(number);
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) return true;
+        String number = strNumber.replace(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+]");
+    }
+}
