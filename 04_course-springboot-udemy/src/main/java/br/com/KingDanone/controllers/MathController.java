42096b54fb47b54bb07d09c7613f729767897e9f
KingDanone <lricardolv10@gmail.com>
2025-06-24 12:42:40 -0300

    adpter for the structure controler -> service

--- 04_course-springboot-udemy/src/main/java/br/com/KingDanone/controllers/MathController.java
+++ 04_course-springboot-udemy/src/main/java/br/com/KingDanone/controllers/MathController.java
@@ -2,6 +2,8 @@ package br.com.KingDanone.controllers;
 
 
 import br.com.KingDanone.exception.UnsupportedMathOperationException;
+import br.com.KingDanone.service.MathService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,26 +12,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/math")
 public class MathController {
 
+    private final MathService mathService;
+    public MathController(MathService mathService) {this.mathService = mathService;}
+
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
-    public Double sum(
+    public ResponseEntity<Double> sum(
             @PathVariable("numberOne") String numberOne,
             @PathVariable("numberTwo") String numberTwo
     ) throws Exception{
-        if (isNumeric(numberOne) || isNumeric(numberTwo))
-            throw new UnsupportedMathOperationException("Please set a numeric value!!");
-        return convertDouble(numberOne) + convertDouble(numberTwo);
+        return ResponseEntity.ok(mathService.sum(numberOne, numberTwo));
     }
 
-    private Double convertDouble(String strNumber) throws IllegalArgumentException{
-        if (strNumber == null || strNumber.isEmpty())
-            throw new UnsupportedMathOperationException("Please set a numeric value!!");
-        String number = strNumber.replace(",", ".");
-        return Double.parseDouble(number);
-    }
-
-    private boolean isNumeric(String strNumber) {
-        if (strNumber == null || strNumber.isEmpty()) return true;
-        String number = strNumber.replace(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+]");
-    }
+//    private Double convertDouble(String strNumber) throws IllegalArgumentException{
+//        if (strNumber == null || strNumber.isEmpty())
+//            throw new UnsupportedMathOperationException("Please set a numeric value!!");
+//        String number = strNumber.replace(",", ".");
+//        return Double.parseDouble(number);
+//    }
+//
+//    private boolean isNumeric(String strNumber) {
+//        if (strNumber == null || strNumber.isEmpty()) return true;
+//        String number = strNumber.replace(",", ".");
+//        return number.matches("[-+]?[0-9]*\\.?[0-9]+]");
+//    }
 }
